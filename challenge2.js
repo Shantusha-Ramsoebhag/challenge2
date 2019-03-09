b0340bef57e30e5a4ff955c1f626c8d94e02c9ea
Shantusha-Ramsoebhag <43187015+Shantusha-Ramsoebhag@users.noreply.github.com>
2019-03-09 10:33:30 +0100

    commit 3

--- challenge2.js
+++ challenge2.js
@@ -1,3 +1,4 @@
+//Code voor werkende klok. Waarbij er tegen elke wijzer wordt hoe die moet lopen.
 setInterval(function(){
   var date = new Date();
   updateTime(date);
@@ -16,6 +17,30 @@ function updateClock(date){
 
 function updateTime(date){
   var tijdDiv = document.getElementById("tijd");
-  var tijd = "Time for Reference --- " + date.getHours() + " : " + date.getMinutes() + " : " + date.getSeconds();
-  tijdDiv.innerHTML = tijd;
+  //test 
+  //var tijd = "Tijdaanwijzing --- " + date.getHours() + " : " + date.getMinutes() + " : " + date.getSeconds();
+  //tijdDiv.innerHTML = tijd;
+}
+
+//Om de verschillende elementen op die pagina mooi te laten animeren, gebruik ik een Timeline.
+var tl = new TimelineMax();
+//Hieronder staat de code voor de animatie van de zon en maan wanneer hun tijdszone komt, dus bij dag hoort de zonanimatie te komen en bij nacht de maananimatie.
+//tl.from(".zonfoto", 2, {y:200, opacity:0, scale:0, ease:Bounce.easeOut})
+//tl.from(".maanfoto", 2, {y:200, opacity:0, scale:0, ease:Bounce.easeOut});
+tl.from(".titel", 2, {x:300, ease:Bounce.easeOut })
+  .from("#klok", 3, {y:250, opacity:0, scale:0, ease:Bounce.easeOut}, "-=1.5");
+
+//Om de zon en maan op het juiste moment van de dag op te laten komen maak ik funtion met een if-statement.
+
+function tijdZone(){
+  var tijd = new Date().getHours();
+  var tijdZoneA = Tweenmax.from(".zonfoto", 2, {y:200, opacity:0, scale:0, ease:Bounce.easeOut});
+  var tijdZoneB = Tweenmax.from(".maanfoto", 2, {y:200, opacity:0, scale:0, ease:Bounce.easeOut});
+
+  if (tijd > 7) {
+    tijdZoneA;
+  }
+  else{
+    tijdZoneB;
+  }
 }
