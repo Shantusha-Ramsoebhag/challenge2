cc63ad9a71286e03dcf0c179c0d06bdd08ac460a
Shantusha-Ramsoebhag <43187015+Shantusha-Ramsoebhag@users.noreply.github.com>
2019-03-11 12:57:57 +0100

    commit4

--- challenge2.js
+++ challenge2.js
@@ -1,3 +1,7 @@
+//Code voor datum.
+var date = new Date();
+document.getElementById("datum").innerHTML = date.toDateString();
+
 //Code voor werkende klok. Waarbij er tegen elke wijzer wordt hoe die moet lopen.
 setInterval(function(){
   var date = new Date();
@@ -27,20 +31,40 @@ var tl = new TimelineMax();
 //Hieronder staat de code voor de animatie van de zon en maan wanneer hun tijdszone komt, dus bij dag hoort de zonanimatie te komen en bij nacht de maananimatie.
 //tl.from(".zonfoto", 2, {y:200, opacity:0, scale:0, ease:Bounce.easeOut})
 //tl.from(".maanfoto", 2, {y:200, opacity:0, scale:0, ease:Bounce.easeOut});
-tl.from(".titel", 2, {x:300, ease:Bounce.easeOut })
+tl.from(".titel", 3, {x:300, ease:Bounce.easeOut})
+  .from("#message", 3, {x:-300, ease:Bounce.easeOut}, "-=0,5")
   .from("#klok", 3, {y:250, opacity:0, scale:0, ease:Bounce.easeOut}, "-=1.5");
-
+  
 //Om de zon en maan op het juiste moment van de dag op te laten komen maak ik funtion met een if-statement.
+//function tijdZone(){
+  //var tijd = new Date().getHours();
+  //var tijdZoneA = Tweenmax.from(".zonfoto", 2, {y:200, opacity:0, scale:0, ease:Bounce.easeOut});
+  //var tijdZoneB = Tweenmax.from(".maanfoto", 2, {y:200, opacity:0, scale:0, ease:Bounce.easeOut});
 
-function tijdZone(){
-  var tijd = new Date().getHours();
-  var tijdZoneA = Tweenmax.from(".zonfoto", 2, {y:200, opacity:0, scale:0, ease:Bounce.easeOut});
-  var tijdZoneB = Tweenmax.from(".maanfoto", 2, {y:200, opacity:0, scale:0, ease:Bounce.easeOut});
+  //if (tijd > 7) {
+    //tijdZoneA;
+  //}
+  //else{
+    //tijdZoneB;
+  //}
+//}
 
-  if (tijd > 7) {
-    tijdZoneA;
-  }
-  else{
-    tijdZoneB;
-  }
+const dt = new Date();
+
+let hour = dt.getHours();
+let minutes = dt.getMinutes();
+
+if (hour>=12 && hour<=17){
+  document.getElementById("message").innerHTML = "Good afternoon";
 }
+else if (hour >=17 && hour<=20){
+  document.getElementById("message").innerHTML = "Good evening";
+}
+else if (hour >=20 || hour<=6){
+  document.getElementById("message").innerHTML = "Good night";
+}
+else{
+  document.getElementById("message").innerHTML = "Good morning";
+  document.getElementById("img2").src = "img/zon.png";
+}
+
